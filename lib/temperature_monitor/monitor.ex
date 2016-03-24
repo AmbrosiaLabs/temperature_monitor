c5cc7f19351dd3bf6cab8849a12a694aeacddd69
Steve Newell <steve.newell@mx.com>
2016-03-23 21:52:19 -0600

    get parsing to work

--- lib/temperature_monitor/monitor.ex
+++ lib/temperature_monitor/monitor.ex
@@ -7,7 +7,7 @@ defmodule TemperatureMonitor.Monitor do
 
   def init(stash_pid) do
     filelist = TemperatureMonitor.Stash.get_value stash_pid
-     {:ok, stash_pid}
+     {:ok, {filelist, stash_pid}}
   end
 
   def sample_all do
@@ -22,13 +22,17 @@ defmodule TemperatureMonitor.Monitor do
     GenServer.cast(__MODULE__, {:add_file, filename})
   end
 
-  def handle_call(:sample_all, _from, filelist) do
-    Enum.map(filelist, &read_temperature(&1))
+  def handle_call(:sample_all, _from, {filelist, stash_pid}) do
+    temperatures =
+      filelist
+      |> Enum.map(&read_temperature(&1))
+
+    {:reply, temperatures, {filelist, stash_pid}}
   end
 
-  def handle_call(:sample, _from, filename) do
+  def handle_call({:sample, filename}, _from, {filelist, stash_pid}) do
     temperature = read_temperature(filename)
-    {:reply, temperature, temperature }
+    {:reply, temperature, {filelist, stash_pid} }
   end
 
   def handle_cast({:add_file, filename}, {filelist, stash_pid}) do
@@ -41,7 +45,8 @@ defmodule TemperatureMonitor.Monitor do
     |> Stream.map(&String.strip/1)
     |> Enum.take(2)
 
-    parse_temperature(List.first(lines), List.last(lines))
+    {temperature, _} = parse_temperature(List.first(lines), List.last(lines)) 
+    {filename, temperature / 1000 }
   end
 
   def terminate(_reason, {filelist, stash_pid}) do
@@ -51,7 +56,9 @@ defmodule TemperatureMonitor.Monitor do
   defp parse_temperature(first_string, second_string) do
     cond do 
       String.ends_with?(first_string, "YES") ->
-        String.split(second_string, "=") |> List.last
+        String.split(second_string, "=") 
+        |> List.last 
+        |> Integer.parse 
       true ->
         nil
     end
